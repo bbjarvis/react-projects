5a5ba12118000927a5e00e8f4de4da7f8b471a60
Brett Jarvis <brettbjarvis@gmail.com>
2023-03-14 16:24:35 -0700

    update price element

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -24,30 +24,32 @@ function App() {
 
   return (
     <main>
-      <div className='title'>
-        <h2>Our Tours</h2>
-        <div className='underline'></div>
-      </div>
+      <section>
+        <div className='title'>
+          <h2>Our Tours</h2>
+          <div className='underline'></div>
+        </div>
+        <div>
+          {tours.map((tour) => {
+            const { id, name, info, image, price } = tour
+            return (
+              <section>
+                <article key={id} className='single-tour'>
+                  <img className='single-tour img' src={image} alt={name}></img>
 
-      {tours.map((tour) => {
-        const { id, name, info, image, price } = tour
-        return (
-          <section>
-            <ul key={id} className='single-tour'>
-              <article>
-                <img className='single-tour img' src={image} alt={name}></img>
-                <footer>
-                  <div className='tour-info'>
-                    <h4>{name}</h4>
-                    <h4 className='tour-price'>${price}</h4>
-                  </div>
-                </footer>
-                <p className='tour-info'>{info}</p>
-              </article>
-            </ul>
-          </section>
-        )
-      })}
+                  <footer className='ingle-tour footer'>
+                    <div className='tour-info'>
+                      <h4>{name}</h4>
+                      <h4 className='tour-price'>${price}</h4>
+                    </div>
+                  </footer>
+                  <p className='tour-info'>{info}</p>
+                </article>
+              </section>
+            )
+          })}
+        </div>
+      </section>
     </main>
   )
 }
