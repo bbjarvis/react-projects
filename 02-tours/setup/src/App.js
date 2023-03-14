3d3e1a1676e5a8c4b6c5467cfdc9a353f2c2355d
Brett Jarvis <brettbjarvis@gmail.com>
2023-03-14 15:46:50 -0700

    init tours cards

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,55 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch(url)
+      const tours = await response.json()
+      setTours(tours)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  return (
+    <main>
+      <div className='title'>
+        <h2>Our Tours</h2>
+        <div className='underline'></div>
+      </div>
+
+      {tours.map((tour) => {
+        const { id, name, info, image, price } = tour
+        return (
+          <section>
+            <ul key={id} className='single-tour'>
+              <article>
+                <img className='single-tour img' src={image} alt={name}></img>
+                <footer>
+                  <div className='tour-info'>
+                    <h4>{name}</h4>
+                    <h4 className='tour-price'>${price}</h4>
+                  </div>
+                </footer>
+                <p className='tour-info'>{info}</p>
+              </article>
+            </ul>
+          </section>
+        )
+      })}
+    </main>
+  )
 }
 
 export default App
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
-};
+import React from 'react'
+import Tour from './Tour'
+const Tours = (url) => {
+  return (
+    <>
+      {url.map((singleTour) => {
+        const { id, name, info, image, price } = singleTour
+        return (
+          <h1>tours</h1>
+          // <ul key={id} className='single-tour'>
+          //   <img src={image} alt={name}></img>
+          //   <div>
+          //     <h4>{name}</h4>
+          //     <p>{info} years</p>
+          //   </div>
+          // </ul>
+        )
+      })}
+    </>
+  )
+}
 
-export default Tours;
+export default Tours
