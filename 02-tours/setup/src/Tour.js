02123d6a11871bdd8314c3c9c20afb6fb9c62284
Brett Jarvis <brettbjarvis@gmail.com>
2023-03-15 10:14:15 -0700

    refactor Tours intor Tour component

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const Tour = () => {
-  return <h2>tour component</h2>;
-};
+const Tour = ({ tour }) => {
+  const { id, name, info, image, price } = tour
+  return (
+    <article key={id} className='single-tour'>
+      <img src={image} alt={name}></img>
+      <footer>
+        <div className='tour-info'>
+          <h4>{name}</h4>
+          <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p className='tour-info'>{info}</p>
+      </footer>
+    </article>
+  )
+}
 
-export default Tour;
+export default Tour
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -9,19 +9,7 @@ const Tours = ({ tours }) => {
       </div>
       <div>
         {tours.map((tour) => {
-          const { id, name, info, image, price } = tour
-          return (
-            <article key={id} className='single-tour'>
-              <img src={image} alt={name}></img>
-              <footer>
-                <div className='tour-info'>
-                  <h4>{name}</h4>
-                  <h4 className='tour-price'>${price}</h4>
-                </div>
-                <p className='tour-info'>{info}</p>
-              </footer>
-            </article>
-          )
+          return <Tour tour={tour} />
         })}
       </div>
     </section>
