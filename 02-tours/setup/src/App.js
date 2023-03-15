b464b6495c43ff485661a1cc2412963fe1c68042
Brett Jarvis <brettbjarvis@gmail.com>
2023-03-15 10:09:38 -0700

    refactor 'Our Tours' section into Tours.js

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -24,29 +24,7 @@ function App() {
 
   return (
     <main>
-      <section>
-        <div className='title'>
-          <h2>Our Tours</h2>
-          <div className='underline'></div>
-        </div>
-        <div>
-          {tours.map((tour) => {
-            const { id, name, info, image, price } = tour
-            return (
-              <article key={id} className='single-tour'>
-                <img src={image} alt={name}></img>
-                <footer>
-                  <div className='tour-info'>
-                    <h4>{name}</h4>
-                    <h4 className='tour-price'>${price}</h4>
-                  </div>
-                  <p className='tour-info'>{info}</p>
-                </footer>
-              </article>
-            )
-          })}
-        </div>
-      </section>
+      <Tours tours={tours} />
     </main>
   )
 }
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import Tour from './Tour'
-const Tours = (url) => {
+const Tours = ({ tours }) => {
   return (
-    <>
-      {url.map((singleTour) => {
-        const { id, name, info, image, price } = singleTour
-        return (
-          <h1>tours</h1>
-          // <ul key={id} className='single-tour'>
-          //   <img src={image} alt={name}></img>
-          //   <div>
-          //     <h4>{name}</h4>
-          //     <p>{info} years</p>
-          //   </div>
-          // </ul>
-        )
-      })}
-    </>
+    <section>
+      <div className='title'>
+        <h2>Our Tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          const { id, name, info, image, price } = tour
+          return (
+            <article key={id} className='single-tour'>
+              <img src={image} alt={name}></img>
+              <footer>
+                <div className='tour-info'>
+                  <h4>{name}</h4>
+                  <h4 className='tour-price'>${price}</h4>
+                </div>
+                <p className='tour-info'>{info}</p>
+              </footer>
+            </article>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
